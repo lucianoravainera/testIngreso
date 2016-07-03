79f9937ca97646146b38f2cc806bc1712e2e3db1
lucho ravainera <lucho ravainera>
2016-07-03 03:45:57 -0300

    ejercicios "if" hasta el 6 terminados y funcionando

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,8 +2,13 @@ function Mostrar()
 {
 //tomo la edad  
 var laEdad = document.getElementById('edad').value;
+var elEstadoCivil = document.getElementById('estadoCivil').value;
+
 if(laEdad < 18)	{
-	
+	if(elEstadoCivil <> soltero){ //revisar esta linea
+		alert("Es muy pequeño para no ser soltero");
+	}
+
 }
 	
 
