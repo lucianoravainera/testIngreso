a70b9473f069b61743ef42d43d266a25a54bbc83
lucho ravainera <lucho ravainera>
2016-07-03 02:58:56 -0300

    1

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+	importe = document.getElementById('importe').value;
+	var resultado;
+	resutlado = parseInt(importe) - parseInt(importe) * 0.25;
+
+	document.getElementById('resultado').value = resutlado;
+
 }
