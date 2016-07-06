b043ec4632a356e74241eab4bea277939b74b94b
lucianoravainera <lucho.barracas@hotmail.com>
2016-07-06 20:19:38 -0300

    ej 4 if en clase usando &&: y.

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,15 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
-var laEdad = document.getElementById('edad').value;		
+var laEdad = document.getElementById('edad').value;
+/* Tambien funciona: 
+if (laEdad >= 18) {
+    alert("Usted es mayor de edad");
+if (laEdad < 18){
+	alert("Usted es menor de edad");
+}*/		
 if (laEdad >= 18) {
     alert("Usted es mayor de edad");
 }
 else{
 	alert("Usted es menor de edad");
 }
-/* Tambien funciona: 
-if (laEdad < 18){
-	alert("Usted es menor de edad");
-}*/
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,12 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 var laEdad = document.getElementById('edad').value;		
+/* Funcion: (echo por mi) con un if dentro de otro
 if(laEdad <= 17){
 if(laEdad >= 13){
-	alert("Usted es adolescente")
+	alert("Usted es adolescente");
 }	
 
-}													
-
+}	*/												
+//Clase: && y. Para juntar 2 condiciones
+if(laEdad >=13 && laEdad <=17){
+	alert("Usted es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
