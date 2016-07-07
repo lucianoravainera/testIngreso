763943079bb74e1357133a1ece14560eb2634ea0
lucianoravainera <lucho.barracas@hotmail.com>
2016-07-06 21:53:42 -0300

    IF ej 7 revisado en clase

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,20 +1,34 @@
 function Mostrar()
 {
 //tomo la edad  
-var laEdad = document.getElementById('edad').value;
-if(laEdad >=18)	{
+	var laEdad = document.getElementById('edad').value;
+/* FUNCIONA: echo por mi.
+	if(laEdad >=18)	{
 	alert("Usted es mayor de edad");
-}
-if(laEdad <= 17){
-if(laEdad >= 13){
-	alert("Usted es adolescente")
-}	
+	}
 
-}		
-if(laEdad <13){
+	if(laEdad <= 17){
+		if(laEdad >= 13){
+		alert("Usted es adolescente")
+		}	
+	}
+
+	if(laEdad <13){
 	alert("Usted es un niño");
-}
+	} 
+*/
+// en clase: meyor que el ejemplo anterior.
+	if (laEdad < 13){
+		alert("es menor");
+	}
+	else{
+		if (laEdad >= 18){
+		alert("es mayor");
+		}
+		else{
+			alert("es dolescente");
+		}
 
-
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -3,13 +3,17 @@ function Mostrar()
 //tomo la edad  
 var laEdad = document.getElementById('edad').value;
 var elEstadoCivil = document.getElementById('estadoCivil').value;
-// NO FUNCIONA!!
+/* NO FUNCIONA!!
 if(laEdad < 18)	{
 	if(elEstadoCivil != soltero){ 
 		alert("Es muy pequeño para no ser soltero");
 	}
 
+}*/
+if(laEdad < 18 && elEstadoCivil != "soltero") {
+alert("es muy pequeño para NO ser soltero");
 }
+
 	
 
 
