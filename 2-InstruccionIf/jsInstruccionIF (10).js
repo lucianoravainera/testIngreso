6b307d3b787b580f13792af1a6889ea450f70743
lucianoravainera <lucho.barracas@hotmail.com>
2016-07-13 16:08:08 -0300

    todos ejercicios ifs terminados

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
+/*Al presionar el Botón, asignar una nota RANDOM
+ al examen y mostar: "EXCELENTE" para notas igual
+  a 9 o 10, "APROBÓ" para notas mayores a 4,
+   "Vamos, la proxima se puede" para notas menores a 4 */
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+//Genero el número RANDOM entre 1 y 10 
+var notaRandom = Math.floor((Math.random() * 10) + 1);
+if(notaRandom >= 9)	{
+	alert("EXCELENTE nota: " + notaRandom);
+	}
+else{
+		if(notaRandom < 4){
+		alert("Vamos la proxima se puede, nota: " + notaRandom);	
+		}
+		else{
+			alert("APROBÓ la nota es: " + notaRandom);
+		}
+	}	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
+/*Al presionar el Botón, mostar
+ un número Random del 1 al 10 inclusive */
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+//Genero el número RANDOM entre 1 y 10 
+var numeroRandom = Math.floor((Math.random() * 10) + 1);
+alert(numeroRandom);
 	
 
 }//FIN DE LA FUNCIÓN
