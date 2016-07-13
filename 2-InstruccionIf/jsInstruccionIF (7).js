77336ca93d6fd0fa841f38b1f94b97641be83a2c
lucianoravainera <lucho.barracas@hotmail.com>
2016-07-13 15:51:10 -0300

    ej 8 if ok

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,3 +1,6 @@
+/*Al ingresar una edad menor a 18 años
+ y un estado civil distinto a "Soltero", mostrar el siguiente
+  mensaje: 'Es muy pequeño para NO ser soltero.'*/
 function Mostrar()
 {
 //tomo la edad  
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,15 @@
+/*Al ingresar una edad menor a 18 años y 
+un estado civil distinto a "Soltero", NO HACER NADA, 
+pero si no es asi, y es soltero y no es menor,
+ mostrar el siguiente mensaje: 'Es soltero y no es menor.'*/
 function Mostrar()
 {
 //tomo la edad  
-
-	
+var laEdad = document.getElementById('edad').value;
+var elEstadoCivil = document.getElementById('estadoCivil').value;
+	if(laEdad >= 18 && elEstadoCivil == "Soltero") {
+alert("Es soltero y no es menor");
+}
 
 
 }//FIN DE LA FUNCIÓN
