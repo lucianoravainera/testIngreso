972f4f485114316f77cca2dc7152620accf23583
lucianoravainera <lucho.barracas@hotmail.com>
2016-07-15 21:57:44 -0300

    while ej 6 acumulador.

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,9 @@
 function Mostrar()
 {
-
-	var numero = prompt("ingrese un número entre 0 y 9.");
-
+var numero = prompt("ingrese un número entre 0 y 9.");
+while(numero < 0 || numero > 9){
+	numero = prompt("Ingrese un numero entre el 0 y el 9.");
+								}
+document.getElementById('Numero').value = numero;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,13 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-
-
+	var numero;
+while(contador <5)
+{
+	contador=contador + 1;
+	numero = prompt("ingrese numero: " + contador);
+	acumulador = acumulador + parseInt(numero);
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
