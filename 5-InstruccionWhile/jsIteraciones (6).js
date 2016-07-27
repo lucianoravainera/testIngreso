8cbfbbc1499b29d40c302bd5b36380b67152823f
lucianoravainera <lucho.barracas@hotmail.com>
2016-07-27 20:39:29 -0300

    isNaN para validar si lo ingresado es un numero

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -8,8 +8,13 @@ while(contador <5)
 {
 	contador=contador + 1;
 	numero = prompt("ingrese numero: " + contador);
+	//validacion isNaN para validar si lo ingresado es o no un numero:
+	while(isNaN(numero) == true){
+			numero = prompt("ingrese numero: " + contador);
+			}
 	acumulador = acumulador + parseInt(numero);
 }
+
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
