1fa5ed6a9a20b0df91098b1debcf5ba27d651702
lucianoravainera <lucho.barracas@hotmail.com>
2016-08-02 20:52:00 -0300

    interaciones ej 7 (no funca)

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,11 +4,27 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+   
+//respuesta=prompt("¿Ingresar numero? si/no");
+
+while(respuesta != 'no')
+{
+
+	contador=contador + 1;
+	numero = prompt("ingrese numero: " + contador);
+	//validacion isNaN para validar si lo ingresado es o no un numero:
+	while(isNaN(numero) == true )
+	{
+			numero = prompt("error, ingrese numero: " + contador);
+			}
+	acumulador = acumulador + parseInt(numero);
+	
+}
 
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
-
+ 
 }//FIN DE LA FUNCIÓN
 // hacer: tp 12, 5tp, 7 tp, 9 tp, 11 tp.
-//clase que viene ejercicios de iteraciones (while) con for y fdespues vemos for 
+//clase que viene ejercicios de iteraciones (while) con for y despues vemos for 
